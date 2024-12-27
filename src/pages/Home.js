89f5fc1558464579e2fb37da1e7832dfca322441
Vinmumo <vinnymummo@gmail.com>
2024-12-27 09:10:38 -0500

    home

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container text-center mt-5">
+      <h1>Welcome to My Portfolio</h1>
+      <p className="lead">Showcasing my projects and skills.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div className="container mt-5">
+      <h2>My Projects</h2>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default Portfolio;
